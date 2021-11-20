bd9fed2b775901e76fb9ffaa5002ccbd1e4b5ed6
SM Boolean <samuelemadrigali1991@gmail.com>
2021-11-20 11:48:16 +0100

    commit

--- src/App.vue
+++ src/App.vue
@@ -52,5 +52,5 @@ export default {
 </script>
 
 <style lang="scss">
-
+@import '~@fortawesome/fontawesome-free/css/all.css'; 
 </style>
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -21,7 +21,7 @@ export default {
 		return {
 			flags: ['it', 'en', 'fr']
 		}
-	}
+	},
 }
 </script>
 
